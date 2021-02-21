fb8905266da562fc613ddee2d7d9953350f39eff
Sergey <fangooner.ars@gmail.com>
2021-02-21 10:47:53 +0100

    added type adapter for database

--- lib/models/article.dart
+++ lib/models/article.dart
@@ -1,7 +1,19 @@
+import 'package:hive/hive.dart';
+
+part 'article.g.dart';
+
+@HiveType(typeId: 0)
 class Article {
+  @HiveField(0)
   String title;
+
+  @HiveField(1)
   String text;
+
+  @HiveField(2)
   String url;
+
+  @HiveField(3)
   String publishedDate;
 
   Article({this.title, this.text, this.url, this.publishedDate});
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,21 +4,21 @@ import 'package:news_app/models/article.dart';
 
 class ApiService {
   final String _baseUrl = 'api.nytimes.com';
-  static const String api_key = ''; //  ENTER YOUR API KEY
+  static const String api_key =
+      'G6t8NF8zyen7IxJkC96NJzzRbaqc5An6'; //  ENTER YOUR API KEY
 
   Future<List<Article>> getNews() async {
     Map<String, String> _parameters = {'api-key': api_key};
     String _unencodedPath = '/svc/topstories/v2/home.json';
+    List<Article> list = [];
     try {
       var uri = Uri.http(_baseUrl, _unencodedPath, _parameters);
       var response = await http.get(uri);
       Map<String, dynamic> data = json.decode(utf8.decode(response.bodyBytes));
-      List<Article> list = [];
       data['results'].forEach((article) => list.add(Article.fromMap(article)));
-      return list;
     } catch (err) {
       print(err.toString());
     }
-    return null;
+    return list;
   }
 }
